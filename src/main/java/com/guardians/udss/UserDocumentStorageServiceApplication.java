1ef3a1c9cea578e92f16703b5cbbce376df16fa7
Kiran Rajput <rajputkiran2805@gmail.com>
2024-11-09 22:51:48 +0530

    Application Entry point

---
+++ src/main/java/com/guardians/udss/UserDocumentStorageServiceApplication.java
@@ -0,0 +1,12 @@
+package com.guardians.udss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDocumentStorageServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserDocumentStorageServiceApplication.class, args);
+	}
+
+}
