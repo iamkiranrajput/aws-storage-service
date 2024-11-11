ab8285904cc45b51aa0f445ec37791341b396e38
Kiran Rajput <rajputkiran2805@gmail.com>
2024-11-11 20:09:42 +0530

    updated test cases

--- src/test/java/com/guardians/udss/controller/FileControllerTest.java
+++ src/test/java/com/guardians/udss/controller/FileControllerTest.java
@@ -1,6 +1,7 @@
 package com.guardians.udss.controller;
 
 import com.guardians.udss.controller.FileController;
+import com.guardians.udss.response.ApiResponse;
 import com.guardians.udss.services.S3FileService;
 import com.guardians.udss.services.S3FileServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -46,9 +47,9 @@ class FileControllerTest {
         List<String> mockFiles = Arrays.asList("project1.pdf", "project2.pdf");
 
         when(s3FileService.searchFiles(userName, searchTerm)).thenReturn(mockFiles);
-        ResponseEntity<List<String>> response = fileController.searchFiles(userName, searchTerm);
+        ResponseEntity<ApiResponse<List<String>>> response = fileController.searchFiles(userName, searchTerm);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(mockFiles, response.getBody());
+        assertEquals(mockFiles, response.getBody().getData());
     }
 
     @Test
@@ -58,7 +59,7 @@ class FileControllerTest {
 
         when(s3FileService.searchFiles(userName, searchTerm)).thenThrow(new RuntimeException("Search error"));
 
-        ResponseEntity<List<String>> response = fileController.searchFiles(userName, searchTerm);
+        ResponseEntity<ApiResponse<List<String>>> response = fileController.searchFiles(userName, searchTerm);
 
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
     }
@@ -71,7 +72,7 @@ class FileControllerTest {
         String key = userName + "/" + fileName;
         InputStream mockInputStream = new ByteArrayInputStream("file content".getBytes());
         when(s3FileService.downloadFile(key)).thenReturn(mockInputStream);
-        ResponseEntity<InputStreamResource> response = fileController.downloadFile(userName, fileName);
+        ResponseEntity<ApiResponse<String>> response = fileController.downloadFile(userName, fileName);
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
     }
@@ -85,7 +86,7 @@ class FileControllerTest {
 
         when(s3FileService.downloadFile(key)).thenThrow(new RuntimeException("Download error"));
 
-        ResponseEntity<InputStreamResource> response = fileController.downloadFile(userName, fileName);
+        ResponseEntity<ApiResponse<String>> response = fileController.downloadFile(userName, fileName);
 
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
@@ -98,10 +99,10 @@ class FileControllerTest {
 
         when(s3FileService.uploadFile(userName, file)).thenReturn(fileKey);
 
-        ResponseEntity<String> response = fileController.uploadFile(userName, file);
+        ResponseEntity<ApiResponse<String>> response = fileController.uploadFile(userName, file);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("File uploaded successfully: " + fileKey, response.getBody());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals("File uploaded successfully: " + fileKey, response.getBody().getMessage()+": "+response.getBody().getData());
     }
 
     @Test
@@ -111,32 +112,32 @@ class FileControllerTest {
 
         when(s3FileService.uploadFile(userName, file)).thenThrow(new RuntimeException("Upload error"));
 
-        ResponseEntity<String> response = fileController.uploadFile(userName, file);
+        ResponseEntity<ApiResponse<String>> response = fileController.uploadFile(userName, file);
 
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-        assertEquals("Error uploading file", response.getBody());
+        assertEquals("Failed to upload file. Please try again.", response.getBody().getMessage());
     }
 
     // Test case for deleteFile
     @Test
-    void deleteFile_ShouldReturnSuccessMessage() throws Exception {
+    void deleteFileSuccessMessage() throws Exception {
         String userName = "kiranrana";
         String fileName = "dataengineer.pdf";
 
         doNothing().when(s3FileService).deleteFile(userName, fileName);
-        ResponseEntity<String> response = fileController.deleteFile(userName, fileName);
+        ResponseEntity<ApiResponse<String>> response = fileController.deleteFile(userName, fileName);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("File deleted successfully", response.getBody());
+        assertEquals("File deleted successfully", response.getBody().getMessage());
     }
 
     @Test
-    void deleteFile_ShouldReturnNotFoundOnFailure() throws Exception {
+    void deleteFileNotFoundOnFailure() throws Exception {
         String userName = "kiranrana";
         String fileName = "dataengineer.pdf";
 
         doThrow(new RuntimeException("Delete error")).when(s3FileService).deleteFile(userName, fileName);
-        ResponseEntity<String> response = fileController.deleteFile(userName, fileName);
+        ResponseEntity<ApiResponse<String>> response = fileController.deleteFile(userName, fileName);
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-        assertEquals("Error deleting file", response.getBody());
+        assertEquals("Error deleting file", response.getBody().getMessage());
     }
 }
